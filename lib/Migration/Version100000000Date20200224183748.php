91ac1aa35b7971b4f00d96ac262f36cc89a5d9bb
milospetkovic <elitasoft.web@gmail.com>
2020-02-25 12:58:22 +0100

    migration - added foreign key to delete file shares per user if linked share with user group is unshared

--- lib/Migration/Version100000000Date20200224183748.php
+++ lib/Migration/Version100000000Date20200224183748.php
@@ -41,8 +41,10 @@ class Version100000000Date20200224183748 extends SimpleMigrationStep {
                 'notnull' => false,
                 'length' => 20,
                 'default' => null,
-                'unsigned' => true
+                'unsigned' => false
             ])->setComment('If share is assigned to user group then set flag when cron creates the share for each user in the user group');
+
+            $table->addForeignKeyConstraint('oc_share', [$elbNewColumn], ['id'], ["onDelete" => "CASCADE"], 'frgk_elb_share_for_user_group');
         }
 
         return $schema;
